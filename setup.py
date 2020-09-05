ca375a71bd96e525e11993c8a47fe06ae8000fc6
Vaibhav Rathi <vkrathi722@gmail.com>
2020-09-05 11:01:05 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Yolo in TF2 by VKR',
+    author='Vaibhav Rathi',
+    license='MIT',
+)
